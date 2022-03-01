de3d475448dccffe288ebab939aea44bc3c850dd
sadhu <1714222385@qq.com>
2022-03-01 20:49:04 +0800

    sentence

--- other/en_words.js
+++ other/en_words.js
@@ -13,7 +13,8 @@ const words_2022 = [
 
 const sentences_2022 = [
   "It is hard to wake up someone who is pretending to be asleep.",
-  "Victory is in having done your best. If you've done your best, you've won."
+  "Victory is in having done your best. If you've done your best, you've won.",
+  "A ship is safe in harbor,but that's not what ships are for."
 ]
 
 console.log(`${words_2022.length}/1000`);
