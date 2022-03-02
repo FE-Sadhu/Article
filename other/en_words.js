99c963511a76239810294f1cb5f338a52e3f62b1
sadhu <1714222385@qq.com>
2022-03-02 23:48:26 +0800

    sentence

--- other/en_words.js
+++ other/en_words.js
@@ -17,7 +17,8 @@ const sentences_2022 = [
   "It is hard to wake up someone who is pretending to be asleep.",
   "Victory is in having done your best. If you've done your best, you've won.",
   "A ship is safe in harbor,but that's not what ships are for.",
-  "Shall we go for lunch now?"
+  "Shall we go for lunch now?",
+  "Always believe in yourself. Do this and no matter where you are, you will have nothing to fear."
 ]
 
 console.log(`${words_2022.length}/1000`);
